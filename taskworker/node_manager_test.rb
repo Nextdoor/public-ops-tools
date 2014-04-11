0998a385be5623e39c00cb22d01c2b90766cec83
Wenbin Fang <wenbin@nextdoor.com>
2014-04-10 18:55:48 -0700

    Initial commit for node_manager script.
    The script creates / deletes server array in RightScale.
    
    For server array creation, it clones from a templte server array,
    sets input PUPPET_ADDITIONAL_FACTS, and launches instances.
    
    For server array deletion, it checks if all related SQS queues
    are empty, terminates instances inside the server array, and destroy
    the server array.
    
    Change-Id: I0fb014ac72f729bf3cb6e1269876368dd245ac56

---
+++ taskworker/node_manager_test.rb
@@ -0,0 +1,83 @@
+require 'test/unit'
+require 'node_manager'
+
+class String
+  def code
+    return 200
+  end
+end
+
+class FakeClient
+  def post(post_data, args, &callback)
+    response = '{"access_token": "abcdef" }'
+    callback.call(response, nil, nil)
+  end
+end
+
+class NodeManagerTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_get_release_version
+    release_version = get_release_version(:build_url => 'http://jenkinshost.com/project_20140409-035643%7Erelease-0007a.bb93bbc.dsc')
+    assert(release_version == '20140409-035643~release-0007a.bb93bbc')
+
+    release_version = get_release_version(:build_url => 'http://jenkinshost.com/project_20140409-035643%7Emaster.bb93bbc.dsc')
+    assert(release_version == '20140409-035643~master.bb93bbc')
+  end
+
+  def test_get_queue_prefix
+    queue_prefix = get_queue_prefix(:release_version => '20140409-035643~release-0007a.bb93bbc')
+    assert(queue_prefix == '0007a')
+
+    queue_prefix = get_queue_prefix(:release_version => '20140409-035643~release.bb93bbc')
+    assert(queue_prefix == 'bb93bbc')
+
+    queue_prefix = get_queue_prefix(:release_version => '20140409-035643~master.bb93bbc')
+    assert(queue_prefix == 'bb93bbc')
+  end
+
+  def test_get_server_array_name
+    queue_prefix = get_server_array_name(:env => 'staging',
+                                         :region => 'uswest1',
+                                         :service => 'fe',
+                                         :queue_prefix => '0008a')
+    assert(queue_prefix == 'staging-fe-0008a-uswest1')
+  end
+
+  def test_get_access_token
+    fake_client = FakeClient.new()
+    token = get_access_token(:refresh_token => 'abc',
+                             :client => fake_client,
+                             :api_version => '1.5')
+    assert(token == 'abcdef')
+  end
+
+  def test_create_server_array
+    # TODO (wenbin)
+  end
+
+  def test_find_server_array
+    # TODO (wenbin)
+  end
+
+  def test_are_queues_empty
+    # TODO (wenbin)
+  end
+
+  def test_parse_arguments
+    # TODO (wenbin)
+  end
+
+  def test_delete_server_array
+    # TODO (wenbin)
+  end
+
+  def test_main
+    # TODO (wenbin)
+  end
+end
+
