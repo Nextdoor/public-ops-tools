0ad3ea21ceb984d2d7bd3a470f9783ec387f0f18
Matt Wise <matt@nextdoor.com>
2014-07-01 13:08:26 -0700

    More idempotency in the cowbuilder bootstrap script.
    Change-Id: I09aeff0ab1c0d3346e3a9bc4ef1aa4ed477c14a8

--- jenkins/cowbuilder/bootstrap.sh
+++ jenkins/cowbuilder/bootstrap.sh
@@ -16,5 +16,7 @@
 #   done
 #   sudo bootstrap.sh
 #
-DIST=precise ARCH=amd64 /usr/sbin/cowbuilder --create --configfile cowbuilderrc
-DIST=precise ARCH=amd64 /usr/sbin/cowbuilder --execute --save-after-exec --configfile cowbuilderrc -- ./finish.sh
+if [[ ! -d "/var/cache/pbuilder/precise-amd64-base.cow" ]]; then
+  DIST=precise ARCH=amd64 /usr/sbin/cowbuilder --create --configfile cowbuilderrc
+  DIST=precise ARCH=amd64 /usr/sbin/cowbuilder --execute --save-after-exec --configfile cowbuilderrc -- ./finish.sh
+fi
