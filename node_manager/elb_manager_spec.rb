5d83038e2f5712d74190ac110164b02760b19fb7
Charles McLaughlin <charles@nextdoor.com>
2014-07-08 11:17:11 -0700

    formatting, misplaced end
    Change-Id: Id992ea834c24db2ab21f433bd69bfc66632f8ef2

--- node_manager/elb_manager_spec.rb
+++ node_manager/elb_manager_spec.rb
@@ -3,19 +3,16 @@ require 'elb_manager'
 describe 'update_elb' do
   describe 'update_elb checks' do
 
-#    before :each do
+    before :each do
       # Create a mocked object to track RightScale API calls
-#      @rs_mock = double('RightScale')
-#      allow(@rs_mock).to receive(:new) { @client }
-#    end
+      @rs_mock = double('RightScale')
+      allow(@rs_mock).to receive(:new) { @client }
+    end
 
     it "should raise exception with bad action" do
-      @rs_mock = double('RightScale')
-
       expect {
         update_elb(@rs_mock, 'fake_elb', 'fake_sa', 'fake_url', 'bad' )
       }.to raise_error(/Action must be/)
-      end
     end
 
     it "elb => foo_elb, server_array => foo_sa, action => add" do
@@ -33,12 +30,11 @@ describe 'update_elb' do
 
       sa_mock.should_receive(:multi_run_executable).with(
         :right_script_href => 'fake_url',
-        :inputs => { 'ELB_NAME' => 'text:foo_elb' } ) {
-        task_mock
-      }
+        :inputs => { 'ELB_NAME' => 'text:foo_elb' } ) { task_mock }
 
       update_elb(rs_mock, 'foo_elb', 'foo_sa', 'fake_url', 'add')
   
   # @rs_mock.should_receive(:some_method).once.with(arg1,arg2).and_return('foo')
+    end
   end
 end
