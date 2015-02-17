e2abe0dbcce4c63b3548c4349fefde1c80fb572c
Matt Wise <matt@nextdoor.com>
2015-02-16 18:40:10 -0800

    Explicitly install Docker 1.4.1.
    Issue #4036 (https://github.com/docker/docker/issues/4036) may be
    causing problems with Docker 1.5 and unreliable container executions. We
    explicitly use 1.4.1 in our production systems, so lets do the same
    thing on our Build hosts and see if its more reliable.
    
    Change-Id: I8ff7a7e6b14778321d671d19c9787aa88643e806

--- jenkins/ec2_bootstrap.sh
+++ jenkins/ec2_bootstrap.sh
@@ -273,8 +273,15 @@ install_ruby() {
 }
 
 install_docker() {
-  # Install Docker via the bootstrap script from the Docker team
-  curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+  # Add the repository to your APT sources
+  echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
+  # Then import the repository key
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+  # Install docker
+  apt-get update
+  apt-get install -y lxc-docker-1.4.1
 
   # Ensure that Docker uses /mnt/docker for storage (so it doesn't fill up the
   # root volume). Also ensure that the docker socket file is owned by the
