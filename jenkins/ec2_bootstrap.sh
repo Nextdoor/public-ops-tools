1a214ae36240e84d68dba48cec2ce931a2d5584e
Chuck Karish <chuck@nextdoor.com>
2015-02-28 16:36:20 -0800

    Re-initialize a Jenkins slave after a reboot.
    Change ec2bootstrap.sh to allow calling individual functions.
    Encapsulate data in the functions that use them.
    
    Change-Id: Icabc63622773ff3b223b97f94074973753f22d56

--- jenkins/ec2_bootstrap.sh
+++ jenkins/ec2_bootstrap.sh
@@ -4,22 +4,17 @@
 #
 # This script contains all of the steps we need to launch a completely
 # stock EC2 host and convert it into a Jenkins slave. This includes installing
-# an arbirtary set of packages, custom aptitude repos, ruby, java, etc.
+# an arbitrary set of packages, custom aptitude repos, ruby, java, etc.
 #
 # == Usage
 #
 # Below is a script you could use inside the Jenkins EC2 Instance launcher
-# plugin to configure a host. Take note of the `\n` at the end of ecah newline
-# in the variables below, these are critical.
+# plugin to configure a host.
 #
 #   #!/bin/bash
-#   export HTTPS_REPOS=$"
-#   deb https://s3.amazonaws.com/my.repo.com/repos/precise stable main\n
-#   deb https://s3.amazonaws.com/my.repo.com/repos/precise unstable main\n"
 #
-#   export S3_REPOS=$"
-#   deb s3://ACCESS_KEY:[SECRET_ID]@s3.amazonaws.com/my.repo.com/debian_repos/precise/ stable/\n
-#   deb s3://ACCESS_KEY:[SECRET_iD]@s3.amazonaws.com/my.repo.com/debian_repos/lucid/ stable/\n"
+#   export AWS_ACCESS_KEY_ID=<access_key>
+#   export AWS_SECRET_ACCESS_KEY=<secret_id>
 #
 #   GITHUB="https://raw.githubusercontent.com/Nextdoor/public-ops-tools/master/"
 #   BOOTSCRIPT="${GITHUB}/jenkins/ec2_bootstrap.sh"
@@ -28,60 +23,29 @@
 # == Authors
 #
 # Matt Wise <matt@nextdoor.com>
-#
+# Chuck Karish <chuck@nextdoor.com>
 
-# Exit on any failure.
-set -e
-
-# Be verbose. Wherever we'd print sensitive data, go silent.
-set -x
-
-# Sleep for 3 seconds.. seems that there may be some background things going on
-# when the node first boots up that prevents the apt-installs below to work. Waiting
-# seems seems to help.
-sleep 3
-
-# Discover what version of the OS we're running and set a few variables based
-# on that.
+# Discover what version of the OS we're running so we can set variables based on that.
 source /etc/lsb-release
-if [[ "$DISTRIB_CODENAME" == "trusty" ]]; then
-  RUBYGEM=rubygems-integration
-else
-  RUBYGEM=rubygems
-fi
 
 # Enable unattended use of apt-get.
 export DEBIAN_FRONTEND=noninteractive
 
-# Default list of packages that are installed via APT. These are required to
-# start up Jenkins, and are base packages required for other types of
-# development (like compiling code). Space-separated list.
-DEBIAN_BUILD_PACKAGES=" \
-  cowbuilder \
-  debhelper \
-  quilt"
-
-DEFAULT_PACKAGES=" \
-  ${DEBIAN_BUILD_PACKAGES}
-  default-jre \
+# Packages that are installed on every worker host. Other packages
+# that are needed to fulfill a particular host type's role should
+# be installed by a job that needs them, the first time it runs.
+DEFAULT_PACKAGES="
+  default-jre
   git"
 
-# Ruby and all the Ruby dependencies (RVM is manually installed later
-# automatically by the RVM Jenkins Plugin)
-# (http://stackoverflow.com/questions/9056008/installed-ruby-1-9-3-
-#  with-rvm-but-command-line-doesnt-show-ruby-v/9056395#9056395)
-#
-RUBY_PACKAGES="ruby $RUBYGEM libgdbm-dev bison libffi-dev zlib1g-dev
-               libssl-dev pkg-config"
+# Tools for building .deb archives.
+DEBIAN_BUILD_PACKAGES="
+  debhelper
+  quilt"
 
-# Match the "Remote FS root" setting from the Jenkins EC2 Plugin
-JENKINS_HOME="/mnt/jenkins"
-
-# Used to discover what ephemeral storage volumes were setup for this host.
-METADATA_URL_BASE="http://169.254.169.254/2012-01-12"
-
-# Nextdoor Apt-Key Contents
-NEXTDOOR_APT_KEY="-----BEGIN PGP PUBLIC KEY BLOCK-----
+install_nextdoor_gpg_key() {
+    cat << EOF | apt-key add -
+-----BEGIN PGP PUBLIC KEY BLOCK-----
 Version: GnuPG v1.4.10 (GNU/Linux)
 
 mQENBE75JiUBCACakhf2Hr0nJRJcomZrZO9syWb+dN6efmSb7rYQ7KjfIhTlr5CK
@@ -110,7 +74,9 @@ wRfJCmJ68RCYbKo+3qEGRFrJZXMq8nJS5rCHMAZqI5A/ndUdlP1Db49iDdsk5xSP
 Yhv8t1ZhaKJ0Ij3fiDLgvsYf8ZKJbdqYXo3thVSBvlrOtUJF05Cd0p3FdOKJNrka
 3kfk+gQvanFg6Jj0bzTewl4ukEMjA5DcwOM=
 =pggg
------END PGP PUBLIC KEY BLOCK-----"
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+}
 
 # Set up a couple of misc system settings...
 initial_system_setup() {
@@ -124,19 +90,20 @@ Host *
 EOF
   chown ubuntu.ubuntu ~ubuntu/.ssh/config
 
-  # Install our nextdoor GPG key
-  echo "$NEXTDOOR_APT_KEY" | apt-key add -
+  install_nextdoor_gpg_key
 
   # Do an Apt-Get update so that later package installs can succeed
   apt-get -y -q update
 }
 
 raid_ephemeral_storage() {
+  # This must be run after every reboot.
+
   # If the volume is already set up as an md0 raid, skip this func
   grep -q md0 /etc/mtab && return
 
   # Make sure that mdadm is installed
-  apt-get -y --force-yes -q install mdadm xfsprogs
+  install_packages mdadm xfsprogs
 
   # Configure Raid - take into account xvdb or sdb
   root_drive=`df | awk '$NF=="/" {print $1}'`
@@ -152,6 +119,9 @@ raid_ephemeral_storage() {
   # figure out how many ephemerals we have by querying the metadata API, and then:
   #  - convert the drive name returned from the API to the hosts DRIVE_SCHEME, if necessary
   #  - verify a matching device is available in /dev/
+
+  # Used to discover what ephemeral storage volumes were set up for this host.
+  METADATA_URL_BASE="http://169.254.169.254/2012-01-12"
   drives=""
   ephemeral_count=0
   ephemerals=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/ | grep ephemeral)
@@ -159,21 +129,21 @@ raid_ephemeral_storage() {
     echo "Probing $e .."
     device_name=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/$e)
     # might have to convert 'sdb' -> 'xvdb'
-    device_name=$(echo $device_name | sed "s/sd/$DRIVE_SCHEME/")
-    device_path="/dev/$device_name"
+    device_path=$(sed "s/sd/$DRIVE_SCHEME/" <<< /dev/$device_name)
 
-    # test that the device actually exists since you can request more ephemeral drives than are available
-    # for an instance type and the meta-data API will happily tell you it exists when it really does not.
+    # test that the device actually exists since you can request more ephemeral drives
+    # than are available for an instance type and the meta-data API will happily tell
+    # you it exists when it really does not.
     if [ -b $device_path ]; then
       echo "Detected ephemeral disk: $device_path"
       drives="$drives $device_path"
-      ephemeral_count=$((ephemeral_count + 1 ))
+      ((ephemeral_count ++)) || true
     else
       echo "Ephemeral disk $e, $device_path is not present. skipping"
     fi
   done
 
-  if [ "$ephemeral_count" = 0 ]; then
+  if [[ "$ephemeral_count" = 0 ]]; then
     echo "No ephemeral disk detected. exiting"
     exit 0
   fi
@@ -187,10 +157,12 @@ raid_ephemeral_storage() {
   done
 
   partprobe || true
-  mdadm --create --verbose /dev/md0 --level=0 -c256 --raid-devices=$ephemeral_count $drives ||
+  mdadm --create -v /dev/md0 --level=0 --chunk=256 --raid-devices=$ephemeral_count $drives ||
     true
   echo DEVICE $drives | tee /etc/mdadm.conf
-  { mdadm --detail --scan || true; } | tee -a /etc/mdadm.conf
+  mdadm --detail --scan |
+    awk '/ARRAY/{for (i=0; i<=NF; i++){if (match($i, "^UUID=")){print $1, $2, $i}}}' |
+    tee -a /etc/mdadm.conf
   blockdev --setra 65536 /dev/md0
 
   # Format and mount
@@ -203,33 +175,73 @@ raid_ephemeral_storage() {
 }
 
 prep_for_jenkins() {
-  # Set up a location for Jenkins on the faster instance-storage
+  # Run Jenkins on the faster RAID storage
+  JENKINS_HOME="/mnt/jenkins"  # Match the "Remote FS root" setting from the Jenkins EC2 Plugin
   mkdir -p $JENKINS_HOME && chown ubuntu:ubuntu $JENKINS_HOME
 }
 
+restart_raid_ephemeral_storage() {
+    # docker may have files open on /mnt.
+    service docker stop || true
+    if [[ -f /dev/md0 ]]; then
+        umount /mnt || true
+        mount -t xfs -o noatime /dev/md0 /mnt
+    elif grep -q md127 /proc/mdstat; then
+        # This is a workaround for a bug in the RAID manager.
+        # http://ubuntuforums.org/showthread.php?t=1764861
+        mdadm --stop /dev/md127
+        DRIVES=$(sed -n 's/DEVICE //p' /etc/mdadm.conf)
+        RAID_DEVICES=$(awk '{print NF}' <<< $DRIVES)
+        echo y | mdadm --create -f -v /dev/md0 -l 0 -c 256 --raid-devices=$RAID_DEVICES $DRIVES
+        mount -t xfs -o noatime /dev/md0 /mnt
+    else
+        raid_ephemeral_storage
+        prep_for_jenkins
+    fi
+    service docker start || true
+}
+
 create_apt_sources() {
-  if [[ ! -f '/etc/apt/sources.list.d/https.sources.list' ]]; then
-    # Install the Nextdoor public repos and install the apt-transport-s3 package
-    echo -e "${HTTPS_REPOS}" > /etc/apt/sources.list.d/https.sources.list
 
-    # Explicitly install the apt-transport-s3 package from our public repo first
-    apt-get -y -q update
-  fi
+    # Cause apt commands to Just Do It, even if we don't pass the "-y" flag.
+    # This is needed to run a command (mk-build-deps) that doesn't let us pass "-y".
+    if [[ ! -f /etc/apt/apt.conf.d/30apt_assume_yes.conf ]]; then
+        cat > /etc/apt/apt.conf.d/30apt_assume_yes.conf << EOF
+APT {
+       Get {
+                Assume-Yes "true";
+        };
+};
+EOF
+    fi
 
-  # Install the apt-transport-s3 driver if it iss missing
-  dpkg --status apt-transport-s3 > /dev/null ||
-      apt-get -y --force-yes -q install apt-transport-s3
+    if [[ ! -f '/etc/apt/sources.list.d/https.sources.list' ]]; then
+        # Install the Nextdoor public repos
+        cat > /etc/apt/sources.list.d/https.sources.list << EOF
+deb https://s3.amazonaws.com/cloud.nextdoor.com/repos/precise stable main
+deb https://s3.amazonaws.com/cloud.nextdoor.com/repos/precise unstable main
+EOF
+        apt-get -y -q update
+    fi
 
-  # Now, install the apt-transport-s3 backed repos
-  if [[ ! -f '/etc/apt/sources.list.d/s3.sources.list' ]]; then
-    set +x
-    echo -e "${S3_REPOS}" > /etc/apt/sources.list.d/s3.sources.list
-    set -x
-    apt-get -y -q update
-  fi
+    # Install the apt-transport-s3 driver if it is missing
+    dpkg --status apt-transport-s3 > /dev/null || install_packages apt-transport-s3
 
-  # Configure apt pinning
-  cat > /etc/apt/preferences.d/stable.pref <<EOF
+    # Now, install the apt-transport-s3 backed repos
+    if [[ ! -f '/etc/apt/sources.list.d/s3.sources.list' ]]; then
+        set +x
+        cat > /etc/apt/sources.list.d/s3.sources.list << EOF
+deb s3://${AWS_ACCESS_KEY_ID}:[${AWS_SECRET_ACCESS_KEY}]@s3.amazonaws.com/cloud.nextdoor.com/debian_repos/precise/ stable/
+deb s3://${AWS_ACCESS_KEY_ID}:[${AWS_SECRET_ACCESS_KEY}]@s3.amazonaws.com/cloud.nextdoor.com/debian_repos/precise/ unstable/
+deb s3://${AWS_ACCESS_KEY_ID}:[${AWS_SECRET_ACCESS_KEY}]@s3.amazonaws.com/cloud.nextdoor.com/debian_repos/melissadata/ stable/
+deb s3://${AWS_ACCESS_KEY_ID}:[${AWS_SECRET_ACCESS_KEY}]@s3.amazonaws.com/cloud.nextdoor.com/debian_repos/melissadata/ unstable/
+EOF
+        set -x
+        apt-get -y -q update
+    fi
+
+    # Configure apt pinning
+    cat > /etc/apt/preferences.d/stable.pref <<EOF
 # stable
 Explanation: : stable
 Package: *
@@ -237,7 +249,7 @@ Pin: release n=stable
 Pin-Priority: 1100
 EOF
 
-  cat > /etc/apt/preferences.d/unstable.pref <<EOF
+    cat > /etc/apt/preferences.d/unstable.pref <<EOF
 # unstable
 Explanation: : unstable
 Package: *
@@ -247,8 +259,8 @@ EOF
 }
 
 install_packages() {
-  # Install all of the required default packages via aptitude
-  apt-get -y --force-yes -q install $DEFAULT_PACKAGES
+  # Install a list of Debian packages
+  apt-get -y --force-yes -q install $*
 }
 
 install_ruby() {
@@ -256,13 +268,30 @@ install_ruby() {
   su -l ubuntu -c bash -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
 
   # Set up Ruby, but explicitly uninstall RVM. This lets Jenkins handle the install
-  # of RVM. Note, if the package doesn't exist, we don't care if the uninstall fails.
+  # of RVM. If the package doesn't exist, we don't care if the uninstall fails.
+  # (http://stackoverflow.com/questions/9056008/installed-ruby-1-9-3-
+  #  with-rvm-but-command-line-doesnt-show-ruby-v/9056395#9056395)
   set +e
   apt-get --purge -y --force-yes remove ruby-rvm
   set -e
 
+  if [[ "$DISTRIB_CODENAME" == "trusty" ]]; then
+    RUBYGEM=rubygems-integration
+  else
+    RUBYGEM=rubygems
+  fi
+
   # Install some ruby packages
-  apt-get -y --force-yes -q install $RUBY_PACKAGES
+  RUBY_PACKAGES="
+      ruby
+      $RUBYGEM
+      bison
+      libffi-dev
+      libgdbm-dev
+      libssl-dev
+      pkg-config
+      zlib1g-dev"
+  install_packages $RUBY_PACKAGES
 
   # Su back to the Ubuntu user and install RVM under it
   su -l ubuntu -c bash -c "curl -sSL https://get.rvm.io | bash -s stable --ruby"
@@ -281,11 +310,11 @@ install_docker() {
   #
   # (Note: Our Puppet module already does this, so this is
   # just mimicking production)
-  apt-get install -y linux-image-extra-$(uname -r)
+  install_packages linux-image-extra-$(uname -r)
 
   # Install docker
   apt-get update
-  apt-get install -y lxc-docker-1.4.1
+  install_packages lxc-docker-1.4.1
 
   # Ensure that Docker uses /mnt/docker for storage (so it doesn't fill up the
   # root volume). Also ensure that the docker socket file is owned by the
@@ -301,14 +330,52 @@ EOF
   service docker restart
 }
 
+prepare_cowbuilder() {
+    local BASE="${GITHUB}/jenkins/cowbuilder"
+    local FILES="bootstrap.sh cowbuilderrc finish.sh"
+
+    install_packages cowbuilder
+    cd /tmp
+    for file in $FILES; do
+        curl --silent --insecure -O ${BASE}/${file}
+    done
+    /bin/bash ./bootstrap.sh
+}
+
 function main() {
-  initial_system_setup
-  raid_ephemeral_storage
-  prep_for_jenkins
-  create_apt_sources
-  install_packages
-  install_ruby
-  install_docker
+    # Exit on any failure.
+    set -e
+
+    # Be verbose. Wherever we'd print sensitive data, we'll go silent.
+    set -x
+
+    # Sleep for 3 seconds. seems that there may be some background things going on
+    # when the node first boots up that prevents the apt-installs below to work. Waiting
+    # seems seems to help.
+    sleep 3
+
+    # Optional arguments: names of functions to run.
+    if [[ $# -gt 0 ]]; then
+        for arg in $*; do
+            declare -F $arg > /dev/null || { echo "$arg: not recognized."; exit 1; }
+        done
+
+        for arg in $*; do
+            echo $arg
+            eval $(echo $arg)
+        done
+        exit
+    fi
+
+    initial_system_setup
+    raid_ephemeral_storage
+    prep_for_jenkins
+    create_apt_sources
+    install_packages $DEFAULT_PACKAGES
+    install_packages $DEBIAN_BUILD_PACKAGES
+    install_ruby
+    install_docker
+    if [[ -n "$PREPARE_COWBUILDER" ]]; then prepare_cowbuilder; fi
 }
 
 main $*
